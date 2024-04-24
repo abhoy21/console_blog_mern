e0bffbc9bf8e935ee109cae51ae54dff1ef5d8b3
Abhoy Sarkar <sarkar.ab07@gmail.com>
2024-04-24 21:19:13 +0530

    logout functionality changed

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -16,7 +16,7 @@ router.get("/test", (req, res) => {
 
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post("/signout", signout);
+// router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
 router.get("/:userId", getUser);
 
